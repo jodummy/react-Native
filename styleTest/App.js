898cb41b1ee7dd5ed790ce6bf6fb8682c79f9490
jjs <koogy158@gmail.com>
2019-01-31 14:35:56 +0900

    2019-01-31

--- styleTest/App.js
+++ styleTest/App.js
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-//import styles from "./ComponentName/styles";
+import styles from "./ComponentName/styles";
 
-export default class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <View style={styles.parent}>
-        <Text style={styles.child}>child one</Text>
-        <Text style={styles.child}>child two</Text>
-        <Text style={styles.child}>child three</Text>
+        <View style={styles.topBlock}>
+          <View style={styles.leftCol}>
+            <View style={[styles.cellOne, styles.base]} />
+            <View style={[styles.cellTwo, styles.base]} />
+          </View>
+          <View style={[styles.cellThree, styles.base]} />
+        </View>
+        <View style={styles.bottomBlock}>
+          <View style={[styles.cellFour, styles.base]} />
+          <View style={[styles.cellFive, styles.base]} />
+          <View style={styles.bottomRight}>
+            <View style={[styles.cellSix, styles.base]} />
+            <View style={[styles.cellSeven, styles.base]} />
+          </View>
+        </View>
       </View>
     );
   }
 }
 
-
-const styles = StyleSheet.create({
-  parent: {
-    backgroundColor: '#F5FCFF',
-    borderColor: '#0099AA',
-    borderWidth: 5,
-    marginTop: 30
-  },
-  child: {
-    backgroundColor: '#AA0099',
-    borderWidth: 2,
-    textAlign: 'center',
-    fontSize: 24
-  }
-
-})
+export default App;
